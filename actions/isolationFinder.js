ea82014ff160258e9943fb8a913536c3b2bdfbe7
erezt.dev <erezt.dev@gmail.com>
2019-02-27 13:44:38 +0200

    implemented isolated country logic in module, returning object.

--- actions/isolationFinder.js
+++ actions/isolationFinder.js
@@ -1,3 +1,28 @@
 module.exports = function (agents) {
-return agents;
+    const getIsolatedAgentsArray = (data) => {
+        //fetching and counting duplicate agents which signifies !isolated.
+        const getDupes = data.reduce((accumulator, {agent}) => {
+            //start to accumulate agent key:value and count occurrences
+            accumulator[agent] = (accumulator[agent] || 0) + 1;
+            return accumulator;
+        }, {});
+
+        return data.filter(({agent}) => {
+            return getDupes[agent] === 1;
+        });
+    };
+
+    //now we have all isolated agent objects set and we can start counting
+    // their respective countries.
+    // let isolatedAgents = getIsolatedAgentsArray(agents);
+    // console.log("CHECK",isolatedAgents);
+    const getIsolatedCountries = (isolatedAgents => {
+        return isolatedAgents.reduce((accumulator, {country}) => {
+            // console.log(accumulator, country);
+            accumulator[country] = accumulator[country] ? accumulator[country] + 1 : 1;
+            return accumulator;
+        }, {});
+    });
+    // console.log(getIsolatedAgentsArray(agents));
+    return getIsolatedCountries(getIsolatedAgentsArray(agents));
 };
