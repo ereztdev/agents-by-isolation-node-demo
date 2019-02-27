167558daaaba6f8d8a4dc1f57de4f8283df67dc2
erezt.dev <erezt.dev@gmail.com>
2019-02-27 12:54:53 +0200

    module created. next: implement isolated country logic in module

---
+++ actions/isolationFinder.js
@@ -0,0 +1,3 @@
+module.exports = function (agents) {
+return agents;
+};
--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
+
+const isolatedCountries = require('./actions/isolationFinder');
 
 app.get('/', (req, res) => {
     res.send('root empty on purpose ("PONG")')
 });
 
 app.get('/countries-by-isolation', (req, res) => {
-console.log('test');
+    let agentArray = [];
+    //get our array of agent objects
+    fs.readFile('src/agents.json', 'utf8', function (err, data) {
+        if (err) throw err;
+        agentArray = JSON.parse(data);
+        res.setHeader('Content-Type', 'application/json');
+        // res.send(JSON.stringify(obj, null, 3));
+        res.send(isolatedCountries(agentArray));
+        res.end();//closing response flow for now, remove if we want to develop the async fs response further
+    });
 });
 
 app.listen(3000, () => console.log('here'));
